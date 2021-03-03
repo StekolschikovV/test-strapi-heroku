5ad4642ac64e4b0ee1064d7d8a01e44436307008
mk <v.a.stekolschikov@gmail.com>
2021-03-04 00:03:09 +0200

    Update database config

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1ac3856b5361e466f8ba4830ecce65df'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '450e857d-0369-4fb2-9de0-88c7b2078ac3'
+};
