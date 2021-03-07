abd31630f4b2e234490f91986508a2e40aee3c56
mk <v.a.stekolschikov@gmail.com>
2021-03-07 21:20:43 +0200

    add db conditions

--- config/database.js
+++ config/database.js
@@ -1,25 +1,44 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false
-        }
+module.exports = ({ env }) => {
+  if (process.env.NODE_ENV === 'development') {
+    return ({
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-      options: {
-        ssl: true,
+    })
+  } else {
+    const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+            ssl: {
+              rejectUnauthorized: false
+            }
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-    },
-  },
-});
+    }
+  }
+};
